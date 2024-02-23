7f3b215c77248bd783b0c23dd92816b491683398
anestis <anestis.tsolakidis@live.de>
2024-02-23 21:23:46 +0100

    Initial Commit

---
+++ FireFood-/FireFood_App.swift
@@ -0,0 +1,17 @@
+//
+//  FireFood_App.swift
+//  FireFood-
+//
+//  Created by Anestis Tsolakidis on 23.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FireFood_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
